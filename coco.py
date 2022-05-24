5fb5485abafb95b50ebfca90672b2e48186edba8
Matthew Hyatt <mhyatt000@gmail.com>
2022-05-24 12:44:20 -0400

    deploy

---
+++ coco.py
@@ -0,0 +1,4 @@
+'''
+pull coco dataset from the internet with coco.sh
+and study it to find relevant pixels
+'''
---
+++ coco.sh
@@ -0,0 +1,4 @@
+# Download COCO 2017:
+wget http://images.cocodataset.org/zips/train2017.zip
+wget http://images.cocodataset.org/zips/val2017.zip
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
